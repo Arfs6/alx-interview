7974656fad990011c7b7da16a610db09186c5271
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2023-12-06 21:52:56 +0100

    fixed task 0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -18,11 +18,26 @@ def canUnlockAll(boxes):
         return False
     elif not boxes[0] and len(boxes) == 1:  # All boxes opened!
         return True
-    keys = set(boxes[0])
-    keys.add(0)  # First box is already opened
-    for num, box in enumerate(boxes):
-        if num not in keys:
-            return False
-        keys.update(box)
+    knownKeys = set()
+    openedBoxes = [False for i in boxes]
+    openBox(0, knownKeys, openedBoxes, boxes)
+    return all(openedBoxes)
 
-    return True
+
+def openBox(num, knownKeys, openedBoxes, boxes):
+    """Open a box and use the keys in it to open the boxes the keys can open.
+    Parameters:
+    - num: The number of box to open.
+    - knownKeys: A set containing all the keys we have.
+    - openedBoxes: A list of all opened boxes represented by bools.
+    - boxes: All boxes.
+    """
+    if num + 1 > len(boxes):
+        return
+    elif openedBoxes[num]:
+        return
+    openedBoxes[num] = True
+    keys = boxes[num]
+    knownKeys.update(keys)
+    for key in keys:
+        openBox(key, knownKeys, openedBoxes, boxes)
